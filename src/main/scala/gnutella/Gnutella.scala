aee2082fdd314e51d696916fec52210f14fc15c0
Ogasawara <oga.ivc.s27@gmail.com>
2015-05-09 14:42:05 +0900

    create main object for test.

--- src/main/scala/gnutella/Gnutella.scala
+++ src/main/scala/gnutella/Gnutella.scala
@@ -1,5 +1,14 @@
 package gnutella
 
+import java.net.{Socket, InetAddress}
+
+import actor._
+import akka.actor.{ActorRef, Props, ActorSystem}
+import descriptor.PingDescriptor
+import util.Logger
+
+import scala.util.Try
+
 /**
  *
  * Created by Junya on 15/05/07.
@@ -12,8 +21,64 @@ object Gnutella {
   def isWaitingPong = { gnutellaStatus == GnutellaStatus.waitingPong }
 
   def main(args: Array[String]): Unit = {
+    println("Welcome to gnutella!!!")
+    setUp()
 
   }
+
+
+  private def setUp(): Unit = {
+    val connectionManager = setUpActors()
+
+    println("Connect other servent?(y/n)")
+    val c = scala.io.StdIn.readChar()
+    if (c.equals('n'))
+      return
+
+    setUpFirstConnection(connectionManager)
+    gnutellaStatus = GnutellaStatus.waitingPong
+    sendPing(connectionManager)
+    Logger.info("setup finished")
+  }
+
+  /**
+   *
+   * @return ConnectionManager
+   */
+  private def setUpActors(): ActorRef = {
+    val system = ActorSystem("gnutellaActorSystem")
+    val manager = system.actorOf(Props[ConnectionManagerActor], ConnectionManagerActor.name)
+    system.actorOf(Props[ServentsManagerActor], ServentsManagerActor.name)
+    val listen = system.actorOf(Props[ListenConnectionActor], ListenConnectionActor.name)
+    listen ! ListenConnectionActor.ListenConnection
+
+    manager
+  }
+
+  private def setUpFirstConnection(connectionManager: ActorRef): Unit = {
+    print("please input IPAddress: ")
+    val ipAddress = InetAddress.getByName(scala.io.StdIn.readLine())
+    print("please input port: ")
+    val port = scala.io.StdIn.readShort()
+
+    Try( new Socket(ipAddress, port) ).toOption match {
+      case Some(s) => connectionManager ! RunConnectionActor(s)
+      case None => fatal("cannot create socket.")
+    }
+  }
+
+  private def sendPing(connectionManager: ActorRef): Unit = {
+    val ping = new PingDescriptor
+    ping.ttl = 7
+    ping.hops = 0
+    connectionManager ! SendMessageToAllConnections(ping)
+  }
+
+  private def fatal(msg: String): Unit = {
+    println("fatal!!!!!!!!!!!!\n" + msg)
+    System.exit(1)
+  }
+
 }
 
 object GnutellaStatus {
